5748604cbc1f1b33f750c3696c7fa8067165966a
Puiu Rares Gabriel <puiu.raresgabriel.studente@itispaleocapa.it>
2024-09-28 17:20:45 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   console.log('Base URL in App:', import.meta.env.VITE_BASE_URL); // Check the value in the App
   return (
     <>
-      <p>Base URL: {import.meta.env.VITE_BASE_URL}</p>
+      <p>Test: {import.meta.env.VITE_BASE_URL}</p>
     </>
   );
 }
