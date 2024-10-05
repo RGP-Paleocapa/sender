58fa10a891bc78829151914308b491fd1ecb9bd4
Puiu Rares Gabriel <puiu.raresgabriel.studente@itispaleocapa.it>
2024-10-05 18:44:37 +0200

    fix: deploy keeps readmefile

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   console.log('Base URL in App:', import.meta.env.VITE_BASE_URL); // Check the value in the App
   return (
     <>
-      <p>code update.: {import.meta.env.VITE_BASE_URL}</p>
+      <p>Deploy and Keep readme: {import.meta.env.VITE_BASE_URL}</p>
     </>
   );
 }
